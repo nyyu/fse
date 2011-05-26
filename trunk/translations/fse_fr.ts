83f0093f0d1334013cb86caf82e62f79ec4efbf5
Valentin.Oudin@gmail.com <Valentin.Oudin@gmail.com@657d0375-14d1-e867-454b-a27e09c3b826>
2011-05-26 20:44:04 +0000

    update trad

--- trunk/translations/fse_fr.ts
+++ trunk/translations/fse_fr.ts
@@ -25,106 +25,114 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="../src/Window.cpp" line="20"/>
-        <location filename="../src/Window.cpp" line="98"/>
-        <location filename="../src/Window.cpp" line="112"/>
+        <location filename="../src/Window.cpp" line="21"/>
+        <location filename="../src/Window.cpp" line="102"/>
+        <location filename="../src/Window.cpp" line="111"/>
+        <location filename="../src/Window.cpp" line="116"/>
+        <location filename="../src/Window.cpp" line="130"/>
         <source>Fragile Save Editor</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="24"/>
+        <location filename="../src/Window.cpp" line="26"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="25"/>
+        <location filename="../src/Window.cpp" line="27"/>
         <source>&amp;Open...</source>
         <translation>&amp;Ouvrir...</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="26"/>
+        <location filename="../src/Window.cpp" line="28"/>
         <source>&amp;Save</source>
         <translation>&amp;Enregistrer</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="28"/>
+        <location filename="../src/Window.cpp" line="30"/>
         <source>Save &amp;As...</source>
         <translation>Enregistrer &amp;sous...</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="40"/>
+        <location filename="../src/Window.cpp" line="44"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="42"/>
+        <location filename="../src/Window.cpp" line="46"/>
         <source>&amp;Edit</source>
         <translation>&amp;Édition</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="43"/>
+        <location filename="../src/Window.cpp" line="47"/>
         <source>&amp;Find...</source>
         <translation>&amp;Rechercher...</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="44"/>
+        <location filename="../src/Window.cpp" line="48"/>
         <source>Find &amp;Next</source>
         <translation>Rechercher le &amp;suivant</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="47"/>
+        <location filename="../src/Window.cpp" line="51"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="48"/>
+        <location filename="../src/Window.cpp" line="52"/>
         <source>&amp;About</source>
         <translation>À &amp;propos</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="49"/>
+        <location filename="../src/Window.cpp" line="53"/>
         <source>About &amp;Qt</source>
         <translation>À propos de &amp;Qt</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="98"/>
+        <location filename="../src/Window.cpp" line="102"/>
         <source>Are you sure you want to quit?</source>
         <translation>Êtes-vous sûr de vouloir quitter ?</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="107"/>
+        <location filename="../src/Window.cpp" line="111"/>
+        <source>FSE Â© Valentin.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/Window.cpp" line="116"/>
+        <source>Enter an asteroid name:</source>
+        <translation>Entrer un nom d&apos;astéroïde :</translation>
+    </message>
+    <message>
+        <location filename="../src/Window.cpp" line="130"/>
+        <source>No match.</source>
+        <translation>Aucun résultat.</translation>
+    </message>
+    <message>
         <source>About</source>
-        <translation>À propos</translation>
+        <translation type="obsolete">À propos</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="107"/>
-        <source>It&apos;s a savegame for fragile allegiance
-simple for now
-by Valentin</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/Window.cpp" line="112"/>
         <source>Enter an asteroid name</source>
-        <translation>Entrer le nom de l&apos;astéroïde</translation>
+        <translation type="obsolete">Entrer le nom de l&apos;astéroïde</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="155"/>
+        <location filename="../src/Window.cpp" line="175"/>
         <source>File loaded</source>
         <translation>Fichier chargé</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="158"/>
+        <location filename="../src/Window.cpp" line="178"/>
         <source>File is not a savegame</source>
         <translation>Le fichier n&apos;est pas une sauvegarde</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="180"/>
+        <location filename="../src/Window.cpp" line="203"/>
         <source>File saved</source>
         <translation>Fichier sauvegardé</translation>
     </message>
     <message>
-        <location filename="../src/Window.cpp" line="247"/>
+        <location filename="../src/Window.cpp" line="277"/>
         <source>&amp;%1 %2</source>
         <translation></translation>
     </message>
